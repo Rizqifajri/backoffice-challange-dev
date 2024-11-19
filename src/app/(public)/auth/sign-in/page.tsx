dfaa3f5513adec5904643c618ed8285674846d95
Rizqi Fajri <rizqifajri51@gmail.com>
2024-11-19 10:28:07 +0700

    fix done

---
+++ src/app/(public)/auth/sign-in/page.tsx
@@ -0,0 +1,20 @@
+import { SignInForm } from "./_components/form-sign-in";
+
+const SignInPage = () => {
+  return (
+    <section
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f9f9f9",
+      }}
+    >
+      <h1>  </h1>
+      <SignInForm />
+    </section>
+  );
+};
+
+export default SignInPage;
